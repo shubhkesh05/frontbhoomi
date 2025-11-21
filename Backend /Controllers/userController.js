836e3616d157f9664501277ebe93fc30896e8aa2
VanshikaVarshney03 <vanshikavarshney0311@gmail.com>
2025-11-21 20:04:13 +0530

    Create userController.js

---
+++ Backend /Controllers/userController.js
@@ -0,0 +1,11 @@
+
+import User from '../models/User.js';
+
+export const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find().select('-__v');
+        res.json(users);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
